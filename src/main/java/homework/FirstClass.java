a68a696459f58933a54fb811c8927d20d176ff84
diana-stepule <104449034+diana-stepule@users.noreply.github.com>
2022-05-16 22:05:11 +0300

    Pievienoju daļu no mājasdarba

--- src/main/java/homework/FirstClass.java
+++ src/main/java/homework/FirstClass.java
@@ -1,2 +1,26 @@
-package homework;public class FirstClass {
+package homework;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+
+        String country = "Spānija";
+        double population = 47.35;
+        int area = 51;
+        String capital = "Madride";
+        String nationalLanguage = "Spāņu valoda";
+        boolean isEuMember = true;
+        char currency = '€';
+
+
+        System.out.println("Valsts nosaukums: " + country);
+        System.out.println("Spānijas iedzīvotāju skaits ir " + population + " miljoni");
+        System.out.println("Spānijas platība ir " + area + " kvadrātmetrs");
+        System.out.println("Spānijas galvaspilsēta ir " + capital);
+        System.out.println("Spānijas oficiāla valoda ir " + nationalLanguage);
+        System.out.println("Vai Spānija ir ES dalībvalsts?");
+        System.out.println(isEuMember);
+        System.out.println("Spānijas valūta ir " + currency);
+
+    }
 }
